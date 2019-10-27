2329f301e79585a59fcd22ce4eea59a36731d776
Stanley REGNARD <sregnard@e2r3p5.42.fr>
2019-10-27 15:58:43 +0100

    no space left lol

--- srcs/classes/map.cpp
+++ srcs/classes/map.cpp
@@ -2,17 +2,15 @@
 
 Map::Map(Point max)
 {
-    this->cells = new Cell **[max.getX()]();
+    this->cells = new Cell **[max.getX()];
     for (int i = 0; i < max.getX(); i++)
     {
-        this->cells[i] = new Cell *[max.getY()]();
+        this->cells[i] = new Cell *[max.getY()];
         for (int j = 0; j < max.getY(); j++)
         {
-            this->cells[i][j] = new Cell [max.getZ()]();
+            this->cells[i][j] = new Cell [max.getZ()];
             for (int k = 0; k < max.getZ(); k++)
-            {
                 this->cells[i][j][k] = Cell(Point(i, j, k));
-            }
         }
     }
 }
